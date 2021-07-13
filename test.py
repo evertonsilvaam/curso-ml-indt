35151176d680f9b7e1e6e4b4d5a0b4e6f8053d7e
Everton da Silva e Silva <everton.silva.am@gmail.com>
2021-07-13 23:30:54 +0000

    normalizar

---
+++ test.py
@@ -0,0 +1,8 @@
+def bootstrap():
+    print("Professor: 'Leandro Escobar'")
+    print("######################### Bootstrap #####################")
+    print("Corrige a qualidade dos dados")
+    print("É um Método")
+    print("Melhor maneira de garantir aceitação")
+
+bootstrap()
